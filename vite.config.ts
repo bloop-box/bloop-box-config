60bfb5f9b2628745c4dc6ede0a4c839a1e2b6692
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2022-10-05 16:54:40 +0200

    feat: add service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import {visualizer} from 'rollup-plugin-visualizer';
 import {defineConfig} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -8,6 +9,32 @@ export default defineConfig({
         react(),
         tsconfigPaths(),
         visualizer(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: [
+                'favicon.ico',
+                'icon-180.png',
+                'mask-icon.svg',
+            ],
+            manifest: {
+                name: 'Boop Box Config',
+                short_name: 'BoopBoxConfig',
+                description: 'Configuration Generator for Boop Boxes',
+                theme_color: '#1976d2',
+                icons: [
+                    {
+                        src: '/icon-192.png',
+                        sized: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icon-512.png',
+                        sized: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
     ],
     build: {
         sourcemap: true,
