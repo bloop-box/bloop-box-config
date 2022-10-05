9266641501eeb3bf982f15d25e2ec7afe2abffb8
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2022-10-05 17:05:47 +0200

    feat(Nfc): add dialog to display writing state

--- src/components/CommandDisplay.tsx
+++ src/components/CommandDisplay.tsx
@@ -1,13 +1,27 @@
-import {Card, CardActionArea, CardContent, CardHeader} from '@mui/material';
+import {
+    Button,
+    Card,
+    CardActionArea,
+    CardContent,
+    CardHeader,
+    Dialog, DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle
+} from '@mui/material';
 import {useSnackbar} from 'notistack';
+import {useState} from 'react';
 
 type Props = {
     command : string;
     parameters ?: unknown[];
 };
 
+const ndefReader = 'NDEFReader' in window ? new NDEFReader() : null;
+
 const CommandDisplay = ({command, parameters} : Props) : JSX.Element => {
     const {enqueueSnackbar} = useSnackbar();
+    const [nfcController, setNfcController] = useState<AbortController | null>(null);
 
     let result = command;
 
@@ -16,18 +30,23 @@ const CommandDisplay = ({command, parameters} : Props) : JSX.Element => {
     }
 
     const handleClick = async () => {
-        if (!('NDEFReader' in window)) {
+        if (!ndefReader) {
             await navigator.clipboard.writeText(result);
             enqueueSnackbar('Copied to clipboard', {variant: 'success'});
             return;
         }
 
-        const ndef = new NDEFReader();
+        const abortController = new AbortController();
+        setNfcController(abortController);
 
         try {
-            await ndef.write(result);
+            await ndefReader.write(result, {signal: abortController.signal});
             enqueueSnackbar('Written to NFC tag', {variant: 'success'});
-        } catch {
+        } catch (error) {
+            if (error instanceof Error && error.name === 'AbortError') {
+                return;
+            }
+
             enqueueSnackbar('Failed to write to NFC tag', {variant: 'error'});
         }
     };
@@ -38,6 +57,23 @@ const CommandDisplay = ({command, parameters} : Props) : JSX.Element => {
                 <CardHeader title="Command String" subheader="Click to copy"/>
                 <CardContent sx={{fontFamily: 'monospace'}}>{result}</CardContent>
             </CardActionArea>
+
+            <Dialog open={nfcController !== null} maxWidth="sm" fullWidth>
+                <DialogTitle>Write NFC Tag</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Hold your device to the NFC tag you want to write to.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => {
+                        nfcController?.abort();
+                        setNfcController(null);
+                    }}>
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Card>
     );
 };
