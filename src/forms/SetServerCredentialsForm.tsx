83798a413d44b47b1cee2c65dbae68f44dcb0606
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2022-10-07 18:28:35 +0200

    feat(SetServerCredentialsForm): add config for hostname and port

--- src/forms/SetServerCredentialsForm.tsx
+++ src/forms/SetServerCredentialsForm.tsx
@@ -7,12 +7,32 @@ type Props = {
 };
 
 const SetServerCredentialsForm = ({command} : Props) : JSX.Element => {
+    const [hostname, setHostname] = useState('');
+    const [port, setPort] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     return (
         <>
             <Stack spacing={2}>
+                <TextField
+                    label="Hostname"
+                    required
+                    value={hostname}
+                    onChange={event => {
+                        setHostname(event.target.value);
+                    }}
+                    inputProps={{inputMode: 'url'}}
+                />
+                <TextField
+                    label="Port"
+                    required
+                    value={port}
+                    onChange={event => {
+                        setPort(event.target.value);
+                    }}
+                    inputProps={{inputMode: 'numeric'}}
+                />
                 <TextField
                     label="Username"
                     required
@@ -31,7 +51,7 @@ const SetServerCredentialsForm = ({command} : Props) : JSX.Element => {
                 />
             </Stack>
 
-            <CommandDisplay command={command} parameters={[username, password]}/>
+            <CommandDisplay command={command} parameters={[hostname, parseInt(port, 10), username, password]}/>
         </>
     );
 };
